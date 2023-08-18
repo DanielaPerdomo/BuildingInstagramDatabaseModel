89b72802b9cec0adaeb54d614adf630329be7f8c
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-08-18 04:37:08 +0000

    Creacion de tablas y relaciones de un modelo de datos de Instagram

--- src/models.py
+++ src/models.py
@@ -7,23 +7,72 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    #Relaciones hacia user
+    follower_id = Column(ForeignKey("user.id"))
+    follower = relationship("User")
+    #Relacion desde User
+    user_id = Column(ForeignKey("user.id"))
+    user = relationship("User")
+    
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250))
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250))
+    #Relacion 1
+    post = relationship("Post")
+    comment = relationship("Comment")
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    url = Column(String(250))
+    #Relacion con Post 
+    post_id = Column(ForeignKey("post.id"))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    #Relacion  con User
+    user_id = Column(ForeignKey("user.id"))
+    comment = relationship("Comment")
+    media = relationship("Media")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    #Relacion con User y Post
+    user_id = Column(ForeignKey("user.id"))
+    post_id = Column(ForeignKey("post.id"))
+
+
+
 
     def to_dict(self):
         return {}
